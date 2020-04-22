720e2a85b687726fdf5accd9a6deb8aac61bc341
gollaavinash1429 <64130012+gollaavinash1429@users.noreply.github.com>
2020-04-22 11:31:26 +0200

    Add files via upload

---
+++ ConsoleRoulette/src/test/java/com/roulette/ConsoleRoulette/ConsoleRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roulette.ConsoleRoulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
